1fbdc690c2d0a308a2ab64371e85edbb1f14b116
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-10-21 22:02:23 +0200

    chore(auth): hidden for now

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { Contacts } from './contacts/contacts';
-import { Login } from './auth/login/login';
-import { Signup } from './auth/signup/signup';
+// import { Login } from './auth/login/login';
+// import { Signup } from './auth/signup/signup';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: Login },
-  { path: 'signup', component: Signup },
-  { path: 'contact', component: Contacts }
+  //   { path: 'login', component: Login },
+  //   { path: 'signup', component: Signup },
+  { path: 'contact', component: Contacts },
 ];
