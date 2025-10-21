c9d61193e7fb39ace9aa3db5f2d37719b1da8edc
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-10-21 18:15:37 +0200

    feat(footer): folder incl. .html, .scss, .ts added and wired

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,17 +2,20 @@ import { Component, signal, OnInit, OnDestroy, inject } from '@angular/core';
 import { DataService } from './services/data.service';
 import { Subscription } from 'rxjs';
 import { Interfaces } from './interfaces/interfaces';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/header/header';
+import { Footer } from './shared/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterOutlet, Footer, Header],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App implements OnInit, OnDestroy {
   private dataService = inject(DataService);
   private subscriptions: Subscription[] = [];
-  
+
   contacts: Interfaces[] = [];
 
   ngOnInit() {
@@ -23,21 +26,21 @@ export class App implements OnInit, OnDestroy {
     const contactsSubscription = this.dataService.getContacts().subscribe({
       next: (data) => {
         this.contacts = data;
-        
-        // Wenn ihr hier loggen wollt, Hier loggen für Firebase DB Data 
-        data.forEach(contact => {
+
+        // Wenn ihr hier loggen wollt, Hier loggen für Firebase DB Data
+        data.forEach((contact) => {
           console.log('Contact:', contact.firstName);
         });
       },
       error: (error) => {
         console.error('Fehler beim Laden der Contacts:', error);
-      }
+      },
     });
     this.subscriptions.push(contactsSubscription);
   }
 
   ngOnDestroy() {
-    this.subscriptions.forEach(subscription => {
+    this.subscriptions.forEach((subscription) => {
       if (subscription && !subscription.closed) {
         subscription.unsubscribe();
       }
