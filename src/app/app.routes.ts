961d68cb275dafe8e39f4b597a070c3c89a4efcc
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-10-21 18:10:31 +0200

    feat(contacts): folder incl. .html, .scss, .ts added and wired

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { Contacts } from './contacts/contacts';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'contact', component: Contacts }];
---
+++ src/app/contacts/contacts.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  templateUrl: './contacts.html',
+  styleUrls: ['./contacts.scss'],
+})
+export class Contacts {}
