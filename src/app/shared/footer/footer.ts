06addba0df197cb2b9da3a3b56b2a21673dce76a
Jonas Stegner <jstjonas@gmx.de>
2025-10-24 00:55:27 +0200

    feat: menu button change function

--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss'
 })
 export class Footer {
+  activeMenu: string = 'contacts';
 
+  setActive(menu: string): void {
+    this.activeMenu = menu;
+  }
+
+  isActive(menu: string): boolean {
+    return this.activeMenu === menu;
+  }
+
+  getImageSrc(menuName: string, defaultImg: string, activeImg: string): string {
+    return this.isActive(menuName) ? activeImg : defaultImg;
+  }
 }
