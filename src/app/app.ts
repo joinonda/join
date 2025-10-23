3ff4c34d6d31ce039231746b79433ef7e5f176a7
Manuel <manuzcore1@gmail.com>
2025-10-23 17:36:19 +0200

    feat/refactor: simplified structure for the app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,4 @@
-import { Component, signal, OnInit, OnDestroy, inject } from '@angular/core';
-import { DataService } from './services/data.service';
-import { Subscription } from 'rxjs';
-import { Interfaces } from './interfaces/interfaces';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
 import { Footer } from './shared/footer/footer';
@@ -12,33 +9,5 @@ import { Footer } from './shared/footer/footer';
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App implements OnInit, OnDestroy {
-  private dataService = inject(DataService);
-  private subscriptions: Subscription[] = [];
-
-  contacts: Interfaces[] = [];
-
-  ngOnInit() {
-    this.loadContacts();
-  }
-
-  private loadContacts() {
-    const contactsSubscription = this.dataService.getContacts().subscribe({
-      next: (data) => {
-        this.contacts = data;
-      },
-      error: (error) => {
-        console.error('Fehler beim Laden der Contacts:', error);
-      },
-    });
-    this.subscriptions.push(contactsSubscription);
-  }
-
-  ngOnDestroy() {
-    this.subscriptions.forEach((subscription) => {
-      if (subscription && !subscription.closed) {
-        subscription.unsubscribe();
-      }
-    });
-  }
+export class App {
 }
