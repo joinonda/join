c5497c98bfff809704f6d1092d91c6b46164fdf7
Mohamed Touzani <mohamed.touzani@gmx.de>
2025-10-29 12:45:54 +0100

    feat(board-header): implement initial BoardHeader component with template and styles

---
+++ src/app/board/board/board.header/board.header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-board.header',
+  imports: [],
+  templateUrl: './board.header.html',
+  styleUrl: './board.header.scss'
+})
+export class BoardHeader {
+
+}
