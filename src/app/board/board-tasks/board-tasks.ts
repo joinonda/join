1ed91e4f097d050299f9b159b35edef7acc68962
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-10-30 14:57:06 +0100

    feat(board-task-dialog): new component added and implemented in board-tasks.ts

--- src/app/board/board-tasks/board-tasks.ts
+++ src/app/board/board-tasks/board-tasks.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TaskBoardCard } from '../board-task-card/board-task-card';
+import { BoardTaskDialog } from '../board-task-dialog/board-task-dialog';
 
 @Component({
   selector: 'app-board-tasks',
   standalone: true,
-  imports: [TaskBoardCard],
+  imports: [TaskBoardCard, BoardTaskDialog],
   templateUrl: './board-tasks.html',
   styleUrl: './board-tasks.scss',
 })
