53c79e2e393b14f44d52bdd26b4c164a2e881c7e
Mohamed Touzani <mohamed.touzani@gmx.de>
2025-10-30 13:11:36 +0100

    feat(board-header): enhance layout and functionality with responsive design and search feature

--- src/app/board/board-header/board-header.ts
+++ src/app/board/board-header/board-header.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-board-header',
   standalone: true,
-  imports: [],
+  imports: [NgIf, FormsModule],
   templateUrl: './board-header.html',
   styleUrl: './board-header.scss',
 })
-export class BoardHeader {}
+export class BoardHeader {
+  isMobile: boolean = false;
+  searchTerm: string = '';
+  hasSearchResults: boolean = true;
+
+  constructor(private router: Router) {
+    this.updateIsMobile();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.updateIsMobile();
+  }
+
+  private updateIsMobile() {
+    this.isMobile = window.innerWidth < 1024;
+  }
+
+  performSearch(): void {
+    const term = this.searchTerm.trim();
+    this.hasSearchResults = true;
+  }
+
+  openAddTaskComponent(): void {
+    this.router.navigateByUrl('/addTask');
+  }
+
+  openAddTaskOverlay(status: string): void {
+    this.router.navigate(['/addTask'], { queryParams: { status } });
+  }
+}
