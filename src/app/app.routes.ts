fb6d80216def89279f6c3d00f0e9132f7e946940
Manuel <manuzcore1@gmail.com>
2025-11-11 15:34:20 +0100

    feat: hide footer and header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,13 @@ import { Contacts } from './contacts/contacts';
 import { Board } from './board/board';
 import { Addtask } from './add-task/add-task';
 import { Summary } from './summary/summary';
-// import { Login } from './auth/login/login';
-// import { Signup } from './auth/signup/signup';
+import { Login } from './auth/login/login';
+import { Signup } from './auth/signup/signup';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/contact', pathMatch: 'full' },
-  //   { path: 'login', component: Login },
-  //   { path: 'signup', component: Signup },
+  { path: 'login', component: Login },
+  { path: 'signup', component: Signup },
   { path: 'summary', component: Summary },
   { path: 'contact', component: Contacts },
   { path: 'board', component: Board },
